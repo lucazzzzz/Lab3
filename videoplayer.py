70cce90bd993a20591bfe643a0c66f3bd0a020a1
Labop2 <labop2@laboratoire.uqac.ca>
2019-10-18 15:08:17 -0400

    Premier essai avec le fichier .py et la librairie tkinter

---
+++ videoplayer.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import myModule
+
+from tkinter import filedialog
+
+class Application():
+	def __init__(self):
+		self.root = tk.Tk()
+		
+		
+		self.root.mainloop()
+
+
+
+
+app = Application()
